5098867e5061007250e86988b2d0b9b9d074239f
ponetefit <rafael.lopez1@bue.edu.ar>
2026-08-17 21:35:28 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,8 @@
-const CACHE_NAME = 'abdomineitor-v1';
+const CACHE_NAME = 'abdomineitor-v2';
 const APP_SHELL = [
   './',
   './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/apple-touch-icon.png'
+  './manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
